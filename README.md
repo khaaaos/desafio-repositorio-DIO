0546f4d44d8f38c6b86a3c5dd49f47caebf8f3d8
[Richard Willer] <[richardwill3r@gmail.com]>
2023-02-24 17:47:56 -0400

    Adicionado script para atualizar Index do README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Desafio de Repositório (Git/Github) - DIO
 Projeto: Desafio de repositório Github
+
+Index:
+.\Git
+.\Git\git-cheat-sheet-education.pdf
+.\Java
+.\Java\Java-CheatSheet_Edureka.pdf
+.\Java\Links.txt
+.\README.md
---
+++ script-index-readme.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[100]:
+
+
+def update_readme():
+    import os
+    repo = r'C:\Users\Willer\Documents\Classroom\Git\desafio-repositorio-DIO'
+    
+    ls = []
+    for root, dirs, files in os.walk(repo, topdown=True):
+        if '.git' in dirs:
+            dirs.remove('.git')
+        for name in files:
+            ls.append(os.path.join(root, name))
+        for name in dirs:
+            ls.append(os.path.join(root, name))
+    
+    ls.sort()
+    folder_files = []
+    for item in ls:
+        folder_files.append(item.replace(repo, '.'))
+
+    res = '\n'.join(folder_files)
+    
+    os.chdir(r'C:\Users\Willer\Documents\Classroom\Jupyter-lab\script readme')
+    with open(file='ls.txt', mode='r', encoding='utf-8') as fp:
+        backup = fp.read()
+    
+    if res != backup:
+        with open(file='ls.txt', mode='w', encoding='utf-8') as fp:
+            fp.write(res)
+        os.chdir(r'C:\Users\Willer\Documents\Classroom\Git\desafio-repositorio-DIO')
+        with open(file='README.md', mode='r', encoding='utf-8') as fp:
+            readme = fp.read()
+    
+        end = readme.find('Index:')
+        readme = readme[:end+(len('Index:'))]
+    
+        with open(file='README.md', mode='w', encoding='utf-8') as fp:
+            fp.write(readme+'\n')
+            fp.write(res)
+    
+    os.chdir(repo)
+    
+    return 1
+    
+
+
+# In[101]:
+
+
+update_readme()
+
